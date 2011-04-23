43deb1f612586e1d1041f755807b713acb97de5e
Ian MacLeod <ian@nevir.net>
2011-04-23 14:15:45 -0700

    Version bump for the bugfix

--- lib/bumbler.rb
+++ lib/bumbler.rb
@@ -1,6 +1,6 @@
 module Bumbler
   # We can be required twice due to the command line require
-  VERSION = '0.1.3' unless self.const_defined? :VERSION
+  VERSION = '0.1.4' unless self.const_defined? :VERSION
   
   autoload :Bundler,  'bumbler/bundler'
   autoload :Hooks,    'bumbler/hooks'
